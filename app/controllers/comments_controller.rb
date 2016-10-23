809b11990a72c9143dae6bd95a1a638ed6ba801d
Takahito Iwase <hito.0525@gmail.com>
2016-10-23 09:58:32 +0900

    DIVE15コメントを動的に削除する処理の実装、コメント編集のリンクを削除、precompile

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,31 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
 
-    # クライアント要求に応じてフォーマットを変更
+    #  respond_toは、クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.json { render :show, status: :created, location: @comment }
+         # JS形式でレスポンスを返します。
+        format.js { render :index }
       else
         format.html { render :new }
+         format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
+        format.json { render :show, status: :delete, location: @comment }
+        # JS形式でレスポンスを返します。
+        format.js { render :index }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
